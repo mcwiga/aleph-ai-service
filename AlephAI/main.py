2130f50938808b0c1d1b675c555c251ce0fbbe08
michael.mcgloin <mcwiga@gmail.com>
2024-03-02 14:54:15 +0000

    Initial commit aleph service

---
+++ AlephAI/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import requests
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+url = "http://localhost:11436/api/generate"
+
+
+@app.route("/chat", methods=["POST"])
+def home():
+    request_body = request.get_json()
+    if request_body:
+        print("Retrieving Ollama results...")
+        llm_result = requests.post(url, json=request_body)
+        return jsonify(llm_result.json()['response'])
+    else:
+        return {"response", "error"}
+
+if __name__ == "__main__":
+    app.run()
